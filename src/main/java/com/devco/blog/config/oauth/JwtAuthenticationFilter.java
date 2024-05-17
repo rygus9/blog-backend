0d0e2378c2523910e8488ee54bf105c60dd5f1d8
Cuzz <rygus9@naver.com>
2024-05-18 01:25:47 +0900

    JWT Provider 설정

--- src/main/java/com/devco/blog/config/oauth/JwtAuthenticationFilter.java
+++ src/main/java/com/devco/blog/config/oauth/JwtAuthenticationFilter.java
@@ -2,13 +2,17 @@ package com.devco.blog.config.oauth;
 
 import java.io.IOException;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
   @Override
