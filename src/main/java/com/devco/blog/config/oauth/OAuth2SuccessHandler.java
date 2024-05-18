44ab5e59a1298f56704b31b3a8ad532272eec329
Cuzz <rygus9@naver.com>
2024-05-18 09:10:17 +0900

    OAuth 토큰 발급 이후 로직 처리

--- src/main/java/com/devco/blog/config/oauth/OAuth2SuccessHandler.java
+++ src/main/java/com/devco/blog/config/oauth/OAuth2SuccessHandler.java
@@ -1,12 +1,39 @@
 package com.devco.blog.config.oauth;
 
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+  private final JwtProvider jwtProvider;
 
+  @Override
+  public void onAuthenticationSuccess(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      Authentication authentication) throws IOException, ServletException {
+    CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+
+    String userId = oAuth2User.getName();
+    String token = jwtProvider.create(userId);
+
+    Cookie cookie = new Cookie("access-token", token);
+    cookie.setHttpOnly(true);
+    cookie.setSecure(false);
+    cookie.setPath("/");
+    cookie.setMaxAge(7 * 60 * 60);
+
+    response.addCookie(cookie);
+    response.sendRedirect("http://localhost:3000/auth/oauth-response/");
+  }
 }
